7c8879e2ed73dde54bfffe282490b1e97a72a927
jinishk2008 <69375516+jinishk2008@users.noreply.github.com>
2020-11-29 17:25:03 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var database;
+var gameState = 0;
+var playerCount, allPlayers;
+var form, game, player;
+
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  game = new Game();
+  game.getState();
+  game.start();
+
+  
+}
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+
+  }
+
+  if(gameState === 1){
+    clear();
+    game.play()
+
+  }
+
+  }
+  
+    
